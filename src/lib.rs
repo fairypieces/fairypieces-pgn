3d2684182a62c548a730932337a602e8a8f83f37
Jakub Hlusička <limeth@protonmail.com>
2021-04-03 03:23:22 +0200

    Add more en-passant tests; make tests more concise; test custom initial board state

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 #![feature(type_ascription)]
 #![feature(format_args_capture)]
 
+use std::fmt::{self, Display};
 use std::io::{self, Read};
 use std::collections::HashSet;
 use fairypieces_engine::{
     board::{Transformation, SquareBoardGeometry, Isometry},
     piece::PieceDefinitionIndex,
-    game::{Game, GameStateDelta, ReversibleGameStateDelta, Outcome, PlayerIndex},
+    game::{Game, GameState, GameStateDelta, ReversibleGameStateDelta, Outcome, PlayerIndex},
     games::international_chess,
     math::{IVec2, IVecComponent},
 };
@@ -54,20 +55,24 @@ pub enum ValidationError {
 }
 
 #[cfg(feature = "concurrency")]
-fn validate_pgn_games_parallel(pgn_games: impl Iterator<Item=Result<PgnGame, ParseError>>) -> impl IndexedParallelIterator<Item=Result<Game<SquareBoardGeometry>, ParseError>> {
+fn validate_pgn_games_parallel(pgn_games: impl Iterator<Item=Result<PgnGame, ParseError>>, initial_state: Option<GameState<SquareBoardGeometry>>) -> impl IndexedParallelIterator<Item=Result<Game<SquareBoardGeometry>, ParseError>> {
     let pgn_games: Vec<_> = pgn_games.collect();
 
-    pgn_games.into_par_iter().map(|result| {
+    pgn_games.into_par_iter().map(move |result| {
+        let initial_state = &initial_state;
+
         result.and_then(|pgn_game| {
-            pgn_game.validate().map_err(Into::into)
+            pgn_game.validate(initial_state.clone()).map_err(Into::into)
         })
     })
 }
 
-fn validate_pgn_games_sequential(pgn_games: impl Iterator<Item=Result<PgnGame, ParseError>>) -> impl Iterator<Item=Result<Game<SquareBoardGeometry>, ParseError>> {
-    pgn_games.map(|result| {
+fn validate_pgn_games_sequential(pgn_games: impl Iterator<Item=Result<PgnGame, ParseError>>, initial_state: Option<GameState<SquareBoardGeometry>>) -> impl Iterator<Item=Result<Game<SquareBoardGeometry>, ParseError>> {
+    pgn_games.map(move |result| {
+        let initial_state = &initial_state;
+
         result.and_then(|pgn_game| {
-            pgn_game.validate().map_err(Into::into)
+            pgn_game.validate(initial_state.clone()).map_err(Into::into)
         })
     })
 }
@@ -108,8 +113,8 @@ fn parse_pgn_games(read: impl Read) -> impl Iterator<Item=Result<PgnGame, ParseE
 ///
 /// When `Some(Err(ParseError::IoError))` is returned, the iterator is terminated and no items will
 /// follow.
-pub fn parse_games_sequential(read: impl Read) -> impl Iterator<Item=Result<Game<SquareBoardGeometry>, ParseError>> {
-    validate_pgn_games_sequential(parse_pgn_games(read))
+pub fn parse_games_sequential(read: impl Read, initial_state: Option<GameState<SquareBoardGeometry>>) -> impl Iterator<Item=Result<Game<SquareBoardGeometry>, ParseError>> {
+    validate_pgn_games_sequential(parse_pgn_games(read), initial_state)
 }
 
 /// Creates a parallel iterator of games of International Chess, verifying the moves.
@@ -117,16 +122,16 @@ pub fn parse_games_sequential(read: impl Read) -> impl Iterator<Item=Result<Game
 /// When `Some(Err(ParseError::IoError))` is returned, the iterator is terminated and no items will
 /// follow.
 #[cfg(feature = "concurrency")]
-pub fn parse_games_parallel(read: impl Read) -> impl IndexedParallelIterator<Item=Result<Game<SquareBoardGeometry>, ParseError>> {
-    validate_pgn_games_parallel(parse_pgn_games(read))
+pub fn parse_games_parallel(read: impl Read, initial_state: Option<GameState<SquareBoardGeometry>>) -> impl IndexedParallelIterator<Item=Result<Game<SquareBoardGeometry>, ParseError>> {
+    validate_pgn_games_parallel(parse_pgn_games(read), initial_state)
 }
 
 /// Like `parse_games_sequential`, but collects all games into a `Vec` and fails early,
 /// discarding games parsed before the error was encountered.
-pub fn parse_games_all_sequential(read: impl Read) -> Result<Vec<Game<SquareBoardGeometry>>, ParseError> {
+pub fn parse_games_all_sequential(read: impl Read, initial_state: Option<GameState<SquareBoardGeometry>>) -> Result<Vec<Game<SquareBoardGeometry>>, ParseError> {
     let mut games = Vec::new();
 
-    for game in parse_games_sequential(read) {
+    for game in parse_games_sequential(read, initial_state) {
         games.push(game?);
     }
 
@@ -136,10 +141,10 @@ pub fn parse_games_all_sequential(read: impl Read) -> Result<Vec<Game<SquareBoar
 /// Like `parse_games_parallel`, but collects all games into a `Vec` and fails early,
 /// discarding games parsed before the error was encountered.
 #[cfg(feature = "concurrency")]
-pub fn parse_games_all_parallel(read: impl Read) -> Result<Vec<Game<SquareBoardGeometry>>, ParseError> {
+pub fn parse_games_all_parallel(read: impl Read, initial_state: Option<GameState<SquareBoardGeometry>>) -> Result<Vec<Game<SquareBoardGeometry>>, ParseError> {
     let mut games = Vec::new();
 
-    for game in parse_games_parallel(read).collect::<Vec<_>>() {
+    for game in parse_games_parallel(read, initial_state).collect::<Vec<_>>() {
         games.push(game?);
     }
 
@@ -329,8 +334,12 @@ pub struct PgnGame {
 }
 
 impl PgnGame {
-    pub fn validate(&self) -> Result<Game<SquareBoardGeometry>, ValidationError> {
-        let mut game = international_chess::GAME.clone();
+    pub fn validate(&self, initial_state: Option<GameState<SquareBoardGeometry>>) -> Result<Game<SquareBoardGeometry>, ValidationError> {
+        let mut game = if let Some(initial_state) = initial_state {
+            Game::new(international_chess::GAME.rules().clone(), initial_state)
+        } else {
+            international_chess::GAME.clone()
+        };
 
         for (index, san_plus) in self.moves.iter().cloned().enumerate() {
             // println!("Validating move: {:?}", san_plus);
@@ -419,8 +428,8 @@ mod tests {
     use fairypieces_engine::board::BoardGeometry;
 
     #[cfg(feature = "concurrency")]
-    fn count_games(read: impl Read) -> (u32, u32) {
-        let (total, successful) = parse_games_parallel(read).enumerate().map(|(index, game)| {
+    fn count_games(read: impl Read, initial_state: Option<GameState<SquareBoardGeometry>>) -> (u32, u32) {
+        let (total, successful) = parse_games_parallel(read, initial_state).enumerate().map(|(index, game)| {
             if let Err(error) = &game {
                 println!("Could not parse game #{index}: {error:?}");
             }
@@ -436,8 +445,8 @@ mod tests {
     }
 
     #[cfg(not(feature = "concurrency"))]
-    fn count_games(read: impl Read) -> (u32, u32) {
-        let (total, successful) = parse_games_sequential(read).enumerate().map(|(index, game)| {
+    fn count_games(read: impl Read, initial_state: Option<GameState<SquareBoardGeometry>>) -> (u32, u32) {
+        let (total, successful) = parse_games_sequential(read, initial_state).enumerate().map(|(index, game)| {
             if let Err(error) = &game {
                 println!("Could not parse game #{index}: {error:?}");
             }
@@ -452,8 +461,8 @@ mod tests {
         (total, successful)
     }
 
-    fn test_parse(read: impl Read) {
-        let (total, successful) = count_games(read);
+    fn test_parse(read: impl Read, initial_state: Option<GameState<SquareBoardGeometry>>) {
+        let (total, successful) = count_games(read, initial_state);
 
         let errors = total - successful;
         let error_percentage = (errors as f64 / total as f64) * 100.0;
@@ -461,132 +470,156 @@ mod tests {
         eprintln!("Invalid games: {errors} out of {total} ({error_percentage:.2}%)");
 
         if errors > 0 {
-            panic!();
+            panic!("Failed to parse {errors} out of {total} games.", errors=errors, total=total);
         }
     }
 
-    #[test]
-    fn validate_resignation() {
-        const PGN_STRING: &str = r#"""
-[Event "Rated Classical game"]
-[Site "https://lichess.org/a9tcp02g"]
-[White "Desmond_Wilson"]
-[Black "savinka59"]
-[Result "1-0"]
-[UTCDate "2012.12.31"]
-[UTCTime "23:04:12"]
-[WhiteElo "1654"]
-[BlackElo "1919"]
-[WhiteRatingDiff "+19"]
-[BlackRatingDiff "-22"]
-[ECO "D04"]
-[Opening "Queen's Pawn Game: Colle System, Anti-Colle"]
-[TimeControl "480+2"]
-[Termination "Normal"]
+    fn test_parse_one(read: impl Read, initial_state: Option<GameState<SquareBoardGeometry>>) -> Game<SquareBoardGeometry> {
+        let game = just_one(parse_games_sequential(read, initial_state)).unwrap();
 
-1. d4 d5 2. Nf3 Nf6 3. e3 Bf5 4. Nh4 Bg6 5. Nxg6 hxg6 6. Nd2 e6 7. Bd3 Bd6 8. e4 dxe4 9. Nxe4 Rxh2 10. Ke2 Rxh1 11. Qxh1 Nc6 12. Bg5 Ke7 13. Qh7 Nxd4+ 14. Kd2 Qe8 15. Qxg7 Qh8 16. Bxf6+ Kd7 17. Qxh8 Rxh8 18. Bxh8 1-0
-"""#;
-
-        test_parse(Cursor::new(PGN_STRING));
+        game.unwrap_or_else(|err| panic!("{:?}", err))
     }
 
-    #[test]
-    fn validate_checkmate() {
-        const PGN_STRING: &str = r#"""
-[Event "Rated Classical game"]
-[Site "https://lichess.org/j1dkb5dw"]
-[White "BFG9k"]
-[Black "mamalak"]
-[Result "1-0"]
-[UTCDate "2012.12.31"]
-[UTCTime "23:01:03"]
-[WhiteElo "1639"]
-[BlackElo "1403"]
-[WhiteRatingDiff "+5"]
-[BlackRatingDiff "-8"]
-[ECO "C00"]
-[Opening "French Defense: Normal Variation"]
-[TimeControl "600+8"]
-[Termination "Normal"]
-
-1. e4 e6 2. d4 b6 3. a3 Bb7 4. Nc3 Nh6 5. Bxh6 gxh6 6. Be2 Qg5 7. Bg4 h5 8. Nf3 Qg6 9. Nh4 Qg5 10. Bxh5 Qxh4 11. Qf3 Kd8 12. Qxf7 Nc6 13. Qe8# 1-0
-"""#;
-
-        test_parse(Cursor::new(PGN_STRING));
+    fn test_parse_one_from_str(read: &str, initial_state: Option<GameState<SquareBoardGeometry>>) -> Game<SquareBoardGeometry> {
+        test_parse_one(Cursor::new(read), initial_state)
     }
 
-    #[test]
-    fn validate_king_side_castle() {
-        const PGN_STRING: &str = r#"""
-1. Nf3 h6 2. g4 g6 3. Bh3 f6 4. O-O
-"""#;
+    fn list_moves(game: &mut Game<SquareBoardGeometry>, from: Option<IVec2>, expected_len: Option<usize>) {
+        let mut moves: Vec<_> = if let Some(from) = from {
+            game.available_moves().moves_from(from).cloned().collect()
+        } else {
+            game.available_moves().moves().cloned().collect()
+        };
 
-        test_parse(Cursor::new(PGN_STRING));
-    }
+        println!("Board:\n{game}", game=SquareBoardGeometry::print(&game));
 
-    #[test]
-    fn validate_queen_side_castle() {
-        const PGN_STRING: &str = r#"""
-1. Na3 a6 2. d4 b6 3. Be3 c6 4. Qd2 d6 5. O-O-O
-"""#;
+        if let Some(expected_len) = expected_len {
+            assert_eq!(expected_len, moves.len());
+        }
 
-        test_parse(Cursor::new(PGN_STRING));
-    }
-
-    #[test]
-    fn validate_promotion_to_queen() {
-        const PGN_STRING: &str = r#"""
-[Termination "Normal"]
-
-1. h4 g5 2. hxg5 Nf6 3. g6 Bh6 4. g7 Be3 5. gxh8=Q+ Ng8 6. Qxg8#
-"""#;
-
-        test_parse(Cursor::new(PGN_STRING));
-    }
-
-    #[test]
-    fn validate_promotion_to_rook() {
-        const PGN_STRING: &str = r#"""
-[Termination "Normal"]
-
-1. h4 g5 2. hxg5 Nf6 3. g6 Bh6 4. g7 Be3 5. gxh8=R+ Ng8 6. Rxg8#
-"""#;
-
-        test_parse(Cursor::new(PGN_STRING));
-    }
-
-    #[test]
-    fn validate_promotion_to_rook_list() {
-        const PGN_STRING: &str = r#"""
-[Termination "Normal"]
-
-1. h4 g5 2. hxg5 Nf6 3. g6 Bh6 4. g7 Bf4
-"""#;
-        let move_from: IVec2 = [6, 6].into();
-
-        let games = parse_games_all_sequential(Cursor::new(PGN_STRING)).unwrap();
-        let mut game = games[0].clone();
-        let moves = game.available_moves().moves_from(move_from).collect::<Vec<_>>();
-
-        assert_eq!(8, moves.len());
+        moves.sort_unstable();
         println!("\nMoves:");
 
         for (index, available_move) in moves.into_iter().enumerate() {
             let mut game = game.clone();
 
-            game.append(available_move.clone()).unwrap();
+            game.append_unchecked_without_evaluation(available_move.clone());
 
             println!("Possible move #{index}:\n{game}", game=SquareBoardGeometry::print(&game));
         }
     }
 
+    fn test_parse_one_from_str_list(read: &str, initial_state: Option<GameState<SquareBoardGeometry>>, from: Option<IVec2>, expected_len: Option<usize>) -> Game<SquareBoardGeometry> {
+        let mut game = test_parse_one_from_str(read, initial_state);
+
+        list_moves(&mut game, from, expected_len);
+
+        game
+    }
+
+    #[test]
+    fn validate_resignation() {
+        test_parse_one_from_str(r#"""
+[Termination "Normal"]
+
+1. d4 d5 2. Nf3 Nf6 3. e3 Bf5 4. Nh4 Bg6 5. Nxg6 hxg6 6. Nd2 e6 7. Bd3 Bd6 8. e4 dxe4 9. Nxe4 Rxh2 10. Ke2 Rxh1 11. Qxh1 Nc6 12. Bg5 Ke7 13. Qh7 Nxd4+ 14. Kd2 Qe8 15. Qxg7 Qh8 16. Bxf6+ Kd7 17. Qxh8 Rxh8 18. Bxh8 1-0
+        """#, None);
+    }
+
+    #[test]
+    fn validate_checkmate() {
+        test_parse_one_from_str(r#"""
+[Termination "Normal"]
+
+1. e4 e6 2. d4 b6 3. a3 Bb7 4. Nc3 Nh6 5. Bxh6 gxh6 6. Be2 Qg5 7. Bg4 h5 8. Nf3 Qg6 9. Nh4 Qg5 10. Bxh5 Qxh4 11. Qf3 Kd8 12. Qxf7 Nc6 13. Qe8# 1-0
+        """#, None);
+    }
+
+    #[test]
+    fn validate_king_side_castle() {
+        test_parse_one_from_str(r#"""
+1. Nf3 h6 2. g4 g6 3. Bh3 f6 4. O-O
+        """#, None);
+    }
+
+    #[test]
+    fn validate_queen_side_castle() {
+        test_parse_one_from_str(r#"""
+1. Na3 a6 2. d4 b6 3. Be3 c6 4. Qd2 d6 5. O-O-O
+        """#, None);
+    }
+
+    #[test]
+    fn validate_promotion_to_queen() {
+        test_parse_one_from_str(r#"""
+[Termination "Normal"]
+
+1. h4 g5 2. hxg5 Nf6 3. g6 Bh6 4. g7 Be3 5. gxh8=Q+ Ng8 6. Qxg8#
+        """#, None);
+    }
+
+    #[test]
+    fn validate_promotion_to_rook() {
+        test_parse_one_from_str(r#"""
+[Termination "Normal"]
+
+1. h4 g5 2. hxg5 Nf6 3. g6 Bh6 4. g7 Be3 5. gxh8=R+ Ng8 6. Rxg8#
+        """#, None);
+    }
+
+    #[test]
+    fn validate_en_passant_valid_regular() {
+        test_parse_one_from_str(r#"""
+1. e4 c6 2. e5 f5 3. exf6
+        """#, None);
+    }
+
+    #[test]
+    fn validate_en_passant_valid_custom_initial_state() {
+        use international_chess::pieces::*;
+        let mut state = international_chess::create_initial_state([
+            [BK, __, __, __, __, __, __, __],
+            [__, __, __, __, __, __, BP, __],
+            [__, __, __, __, __, __, __, __],
+            [__, __, __, __, __, __, __, __],
+            [__, __, __, __, __, __, __, __],
+            [__, __, __, __, __, __, __, __],
+            [__, __, __, __, __, __, __, WP],
+            [WK, __, __, __, __, __, __, __],
+        ]);
+
+        test_parse_one_from_str_list("1. h4 Kb8 2. h5 g5", Some(state), Some([7, 4].into()), Some(2));
+    }
+
+    #[test]
+    #[should_panic(expected = "ValidationError(InvalidMove { index: 6, mv: \"exf6\", ty: Illegal })")]
+    fn validate_en_passant_invalid_late() {
+        test_parse_one_from_str(r#"""
+1. e4 c6 2. e5 f5 3. Ke2 c5 4. exf6
+        """#, None);
+    }
+
+    #[test]
+    #[should_panic(expected = "ValidationError(InvalidMove { index: 4, mv: \"exf6\", ty: Illegal })")]
+    fn validate_en_passant_invalid_small_steps() {
+        test_parse_one_from_str(r#"""
+1. e4 f6 2. e5 f5 3. exf6
+        """#, None);
+    }
+
     #[test]
     fn validate_long() {
-        const PGN_STRING: &str = r#"""
+        test_parse_one_from_str(r#"""
 1. e4 e5 2. Qf3 Qf6 3. Qxf6 Nxf6 4. Nh3 d6 5. Bb5+ Bd7 6. Bxd7+ Nbxd7 7. f3 Be7 8. d3 O-O 9. Nc3 c6 10. Bg5 h6 11. Bxf6 Bxf6 12. O-O-O a6 13. Na4 Bg5+ 14. f4 exf4 15. Nxg5 hxg5 16. h4 f6 17. hxg5 fxg5 18. Rdg1 b5 19. Nc3 b4 20. Ne2 Ne5 21. Rh5 g4 22. Nd4 Rae8 23. c3 bxc3 24. bxc3 Nxd3+ 25. Kc2 Nf2 26. Nxc6 Rxe4 27. g3 f3 28. Nd4 Nh3 29. Nf5 Re2+ 30. Kb3 Rb8+ 31. Ka3 f2 32. Rh1 Rb7 33. Nxd6 Rbb2 34. Nc4 Rxa2+ 35. Kb3 g6 36. Ra5 Rxa5 37. Nxa5 Kg7 38. Rf1 Ng5 39. Nc4 Nf3 40. Ne3 Nd2+ 41. Ka3 Nxf1 42. Nxg4 Re1 43. Nxf2 Ra1+ 44. Kb2 Ra5 45. c4 Nxg3 46. Nd3 Ne4 47. Kc2 g5 48. Ne1 g4 49. Kd3 Nf6 50. Ng2 g3 51. Kd4 Nh5 52. c5 Rxc5 53. Kxc5 a5 54. Ne3 Kf6 55. Kb5 Ke5 56. Kxa5 Ke4 57. Ng2 Kf3 58. Ne1+ Kf2 59. Nd3+ Ke3 60. Ne1 Kf2 61. Nd3+ Kf1 62. Ne5 g2 63. Nf3 Nf4 64. Nd2+ Ke2 65. Nc4 g1=Q 66. Kb5 Qb1+ 67. Kc5 Kd3 68. Ne5+ Ke4 69. Kd6 Qc1 70. Nc6 Qa3+ 71. Kc7 Qc3 72. Kd6 Ng6 73. Kd7 Ne5+ 74. Kd6 Qxc6+ 75. Ke7 Qd7+ 76. Kf6 Qf5+ 77. Ke7 Ng6+ 78. Ke8 Qf8+ 79. Kd7 Ke5 80. Kc7 Qd6+ 81. Kb7 Ke6 82. Ka7 Kd7 83. Kb7 Kd8 84. Ka7 Qc5+ 85. Kb7 Qc7+ 86. Ka6 Kc8 87. Kb5 Ne5 88. Kb4 Qc4+ 89. Ka5 Qa2+ 90. Kb5 Kc7 91. Kb4 Qc4+ 92. Ka3 Qc1+ 93. Kb4 Kc6 94. Kb3 Kc5 95. Ka4 Qc2+ 96. Ka5 Nc4+ 97. Ka6 Qa4+ 98. Kb7 Qc6+ 99. Kb8 Qe8+ 100. Kb7 Nd6+ 101. Kc7 Qc8# 0-1
-"""#;
+        """#, None);
+    }
 
-        test_parse(Cursor::new(PGN_STRING));
+    #[test]
+    fn validate_promotion_to_rook_list() {
+        const PGN_STRING: &str = r#"""
+1. h4 g5 2. hxg5 Nf6 3. g6 Bh6 4. g7 Bf4
+"""#;
+        test_parse_one_from_str_list(PGN_STRING, None, Some([6, 6].into()), Some(8));
     }
 
     #[ignore]
@@ -595,7 +628,7 @@ mod tests {
         const PATH: &str = "resources/lichess_db_standard_rated_2013-01_truncated_5000.pgn";
         let file = File::open(PATH).unwrap();
 
-        test_parse(file);
+        test_parse(file, None);
     }
 
     #[ignore]
@@ -604,7 +637,7 @@ mod tests {
         const PATH: &str = "resources/lichess_db_standard_rated_2013-01_truncated_500.pgn";
         let file = File::open(PATH).unwrap();
 
-        test_parse(file);
+        test_parse(file, None);
     }
 
     #[ignore]
@@ -613,6 +646,6 @@ mod tests {
         const PATH: &str = "resources/lichess_db_standard_rated_2013-01_truncated_50.pgn";
         let file = File::open(PATH).unwrap();
 
-        test_parse(file);
+        test_parse(file, None);
     }
 }
